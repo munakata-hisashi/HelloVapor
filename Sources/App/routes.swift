870ee0e21bc959891a4e5b9fc6b3c658ecddbcae
hisashi.munakata <hisashi.munakata@septeni-original.co.jp>
2024-03-24 10:55:17 +0900

    post and get sample

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -10,14 +10,39 @@ func routes(_ app: Application) throws {
         "Hello Vapor Workshop!"
     }
     
-    app.get("bottles", ":count") { req -> String in
+    app.get("bottles", ":count") { req -> Bottles in
         let count = try req.parameters.require("count", as: Int.self)
-        return "There were \(count) bottles on the wall"
+        return Bottles(count: count)
     }
 
     app.get("hello", ":name") { req -> String in
+        // デフォルトはas String
         let name = try req.parameters.require("name", as: String.self)
         return "Hello \(name)"
     }
+    
+    app.post("bottles") { req -> String in
+        let bottles = try req.content.decode(Bottles.self)
+        return "There were \(bottles.count) bottles"
+    }
+    
+    app.post("user-info") { req -> UserResponse in
+        let user = try req.content.decode(UserInfo.self)
+        return UserResponse(message: "Hello \(user.name), you are \(user.age)")
+    }
+    
     try app.register(collection: TodoController())
+}
+
+struct UserInfo: Content {
+    let name: String
+    let age: Int
+}
+
+struct UserResponse: Content {
+    let message: String
+}
+
+struct Bottles: Content {
+    let count: Int
 }
