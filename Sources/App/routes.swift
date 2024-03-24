b41a65bceadeb61860e37c4592669e13b32e946c
hisashi.munakata <hisashi.munakata@septeni-original.co.jp>
2024-03-24 10:30:28 +0900

    Getting start

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,13 +2,22 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-app.get { req async in
+    app.get { req async in
         "It works!"
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.get("hello", "workshop") { req async -> String in
+        "Hello Vapor Workshop!"
+    }
+    
+    app.get("bottles", ":count") { req -> String in
+        let count = try req.parameters.require("count", as: Int.self)
+        return "There were \(count) bottles on the wall"
     }
 
+    app.get("hello", ":name") { req -> String in
+        let name = try req.parameters.require("name", as: String.self)
+        return "Hello \(name)"
+    }
     try app.register(collection: TodoController())
 }
